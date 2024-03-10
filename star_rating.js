87d8c9584adeaa6c59c0c90637b5f3e5c67de465
Fred Denis <fredjamo68@gmail.com>
2024-03-10 05:10:31 +0300

    initial commit

---
+++ star_rating.js
@@ -0,0 +1,15 @@
+var stars = function () {
+    
+    let star = document.querySelectorAll('input');
+
+    let showValue = document.querySelector('#rating-value');
+
+    for (let i = 0; i < star.length; i++) {
+        star[i].addEventListener('click', function () {
+            i = this.value;
+            showValue.innerHTML = i + " out of 5";
+        })
+    }
+}
+
+stars()
